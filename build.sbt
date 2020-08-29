0add3e7b41e345dee01d4b1c2c7fdd106a5cbc12
PC <prakashchauhan2010@gmail.com>
2020-08-29 15:37:37 +0530

    Scala Spark program to fetch twitter tweets.

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "TwitterStreaming"
+version := "1.0"
+scalaVersion := "2.10.4"
+libraryDependencies ++= Seq(
+"org.apache.spark" %% "spark-core" % "1.5.2",
+"org.apache.spark" %% "spark-streaming" % "1.5.2",
+"org.apache.spark" %% "spark-streaming-twitter" % "1.5.2"
+)
+
+
---
+++ src/main/scala/TwitterPopularTags.scala
@@ -0,0 +1,80 @@
+import org.apache.spark.streaming.{ Seconds, StreamingContext }
+import org.apache.spark.SparkContext._
+import org.apache.spark.streaming.twitter._
+import org.apache.spark.SparkConf
+
+/**
+ * Calculates popular hashtags (topics) over sliding 10 and 60 second windows from a Twitter
+ * stream. The stream is instantiated with credentials and optionally filters supplied by the
+ * command line arguments.
+ *
+ * Run this on your local machine as
+ *
+ * https://themepacific.com/how-to-generate-api-key-consumer-token-access-key-for-twitter-oauth/994/
+ *
+ */
+object TwitterPopularTags {
+  def main(args: Array[String]) {
+
+    StreamingExamples.setStreamingLogLevels()
+
+    //val Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
+
+    val consumerKey = "rrvCDy4csvOKb1k37n9Gr0R5o"
+    val consumerSecret = "RJAeiI1yVCLTRV4o3acX8k7OmYJJn7I4J1YK1sEasxGzgigdw1"
+    val accessToken = "449575729-rilCjnJQ6H4RnVArIdsNS8lc0aiswO1qP0KXtXsU"
+    val accessTokenSecret = "ppjt88D7b6rQgjWWLTdZ3S67aIhawL0iwKzDugoStb2Oz"
+
+    //val filters = args.takeRight(args.length - 4)
+
+    // Set the system properties so that Twitter4j library used by twitter stream
+    // can use them to generat OAuth credentials
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+    val sparkConf = new SparkConf().setAppName("TwitterPopularTags").setMaster("local[10]")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+
+    val stream = TwitterUtils.createStream(ssc, None, Array("covid-19")) // No Authentication
+
+    //stream.print()
+
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+    hashTags.print()
+
+    /*The transform function in Spark streaming allows one to use any of Apache Spark's transformations on the underlying RDDs for the stream.
+			map is used for an element to element transform, and could be implemented using transform. Essentially,
+			map works on the elements of the DStream and transform allows you to work with the RDDs of the DStream.*/
+
+    val topCounts60 = hashTags.map((_, 1))
+      .reduceByKeyAndWindow((x, y) => x + y, Seconds(60))
+      .map(x => x.swap)
+      .transform(_.sortByKey(false))
+
+    topCounts60.print()
+
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })
+
+    //    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+    //      .map { case (topic, count) => (count, topic) }
+    //      .transform(_.sortByKey(false))
+
+    //     Print popular hashtags
+
+    /*topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach { case (count, tag) => println("%s (%s tweets)".format(tag, count)) }
+    })*/
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
